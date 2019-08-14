8d9a7db6950c4110336f0212150f943053994e2d
Iwona Wojtaszek <iwona.wojtaszek97@gmail.com>
2019-08-14 17:21:20 +0200

    Database conf & register & authtentication

--- app.js
+++ app.js
@@ -3,39 +3,72 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+const mongoose = require('./config/database');
+var jwt = require('jsonwebtoken');
 
+// routers 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+require('dotenv').config();
+app.set('secretKey', 'HelpChatRestApi'); 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin' , 'http://localhost:4200');
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append("Access-Control-Allow-Headers", "Origin, Accept,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  res.append('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+app.get('/favicon.ico', function(req, res) {
+  res.sendStatus(204);
+});
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// express doesn't consider not found 404 as an error so we need to handle 404 explicitly
+// handle 404 error
+app.use(function(req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res, next) {
+  console.log(err);
+  if(err.status === 404)
+      res.status(404).json({message: "Not found"});
+  else 
+      res.status(500).json({message: "Something looks wrong :( !!!"});
 });
 
 module.exports = app;
---
+++ app/api/controllers/users.js
@@ -0,0 +1,48 @@
+const userModel = require('../models/users');
+var bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+    create: function(req, res, next) {
+        userModel.create({ 
+                firstname: req.body.firstname, 
+                lastname: req.body.lastname, 
+                email: req.body.email,
+                password: req.body.password }, function (err, result) {
+            if (err) {
+                next(err);
+            } else {
+                res.json({
+                    status: "200", 
+                    message: "User added successfully!", 
+                    data: null
+                });
+            }
+        });
+    },
+
+    authenticate: function(req, res, next) {
+        userModel.findOne({ email:req.body.email }, function(err, userInfo){
+            if (err) {
+                next(err);
+            } else {
+                if(bcrypt.compareSync(req.body.password, userInfo.password)) {
+                    const token = jwt.sign({ id: userInfo._id }, req.app.get('secretKey'), { expiresIn: '24h' });
+                    res.json({
+                        status:"200", 
+                        message: "User found successfully!", 
+                        data:{ 
+                            user: userInfo, 
+                            token:token }
+                    });
+                }else{
+                    res.json({
+                        status:"error", 
+                        message: "Invalid email or password!", 
+                        data:null
+                    });
+                }
+            }
+        });
+    },
+}
