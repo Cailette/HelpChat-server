20b1a3c1c52cdc5e86f00bfdccd949b8dd365a6a
Iwona Wojtaszek <iwona.wojtaszek97@gmail.com>
2019-11-17 15:58:13 +0100

    open/close many pages

--- api/controllers/chats.js
+++ api/controllers/chats.js
@@ -83,6 +83,16 @@ module.exports = {
             agent: chat? chat.agent : null
         });
     },
+    
+    getVisitorAgent: async function(req, res) {
+        let chat = await chatService.findActiveByVisitorId(req.body.id);
+
+        return res.status(200).json({
+            status: 200, 
+            message: "Chats found successfully!", 
+            user: chat? chat.agent : null
+        });
+    },
 
     delete: async function(req, res){
         const chatDeleted = await chatService.delete(req.params.ChatId)
@@ -121,22 +131,4 @@ module.exports = {
             chat: ratedChat
         });
     },
-
-    // messages: async function(req, res) {
-        
-    //     const m1 = await MessageService.create("5db94c43b2972e4040978092", 
-    //     "Lorem 1 ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit. Excepteur sint occaecat cupidatat non proident.", 
-    //     "agent");
-    //     const m4 = await MessageService.create("5db94c43b2972e4040978092", 
-    //     "Lorem 2 ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
-    //     "agent");
-    //     const m5 = await MessageService.create("5db94c43b2972e4040978092", 
-    //     "Ut 3 enim ad minim Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat?", 
-    //     "visitor");
-
-    //     return res.status(200).json({
-    //         status: 200, 
-    //         message: "Chat updated successfully!"
-    //     });
-    // }
 }
--- routes/chats.js
+++ routes/chats.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const chatsController = require('../api/controllers/chats');
 const authorizate = require('../BuisnessLogic/auth/authorizate');
 
+router.get('/agent', authorizate.authorizate, chatsController.getVisitorAgent);
 router.get('/', authorizate.authorizate, chatsController.getActiveByAgentId);
 router.get('/archive', authorizate.authorizate, chatsController.getInactive);
 router.get('/:ChatId', authorizate.authorizate, chatsController.getById);
@@ -11,8 +12,5 @@ router.put('/:ChatId', authorizate.authorizate, chatsController.updateById);
 router.put('/rating/:ChatId', authorizate.authorizate, chatsController.rating);
 router.delete('/:ChatId', authorizate.authorizate, chatsController.delete);
 router.get('/agent/:VisitorId', authorizate.authorizate, chatsController.getAgentByVisitorId);
-
-
-// router.get('/messages', chatsController.messages);
 
 module.exports = router;
