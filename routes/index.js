e215522c2646cbe444398cdd3f4d20beef524e4b
Iwona Wojtaszek <iwona.wojtaszek97@gmail.com>
2019-11-29 14:45:17 +0100

    fix

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.json({"API" : "HelpChat REST API is Working..."});
+  res.send({"API" : "HelpChat REST API is Working..."});
 });
 
 module.exports = router;
